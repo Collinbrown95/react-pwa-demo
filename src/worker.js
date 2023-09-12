bd19f35197827699931f7790ce07860e2dff9256
Collin Brown <collin.j.brown95@gmail.com>
2023-09-12 14:37:28 -0400

    tidy(worker.js): move callback registration to beginning of script

--- src/worker.js
+++ src/worker.js
@@ -4,40 +4,6 @@ import sqlite3InitModule from '@sqlite.org/sqlite-wasm';
 const log = (...args) => postMessage({ type: 'log', payload: args.join(' ') });
 const error = (...args) => postMessage({ type: 'error', payload: args.join(' ') });
 
-// Initialize sqlite DB if it doesn't already exist.
-const start = function (sqlite3) {
-  let db;
-  if ('opfs' in sqlite3) {
-    db = new sqlite3.oo1.OpfsDb('/tb_cases.sqlite3');
-  } else {
-    db = new sqlite3.oo1.DB('/tb_cases.sqlite3', 'ct');
-  }
-  try {
-    db.exec(`
-    CREATE TABLE IF NOT EXISTS tb_cases(case_id, date, status)`
-    );
-  } finally {
-    db.close();
-  }
-};
-
-// Get sqlite3 instance
-
-const sqlite3 = await sqlite3InitModule({
-  print: log,
-  printErr: error,
-}).then((sqlite3) => {
-  console.log('Create sqlite db if not already exists');
-  try {
-    return sqlite3
-  } catch (err) {
-    error(err.name, err.message);
-  }
-});
-
-// run the initial starting script
-start(sqlite3);
-
 const insertNewCase = function (sqlite3, event) {
   let db;
   if ('opfs' in sqlite3) {
@@ -46,7 +12,7 @@ const insertNewCase = function (sqlite3, event) {
     db = new sqlite3.oo1.DB('/tb_cases.sqlite3', 'ct');
   }
   try {
-    const res = db.exec({
+    db.exec({
       sql: 'INSERT INTO tb_cases(case_id, date, status) VALUES (?,?,?)',
       bind: [
         event.data.caseId,
@@ -91,6 +57,40 @@ self.addEventListener('message', async event => {
     error(err.name, err.message);
   }
 });
+
+// Initialize sqlite DB if it doesn't already exist.
+const start = function (sqlite3) {
+  let db;
+  if ('opfs' in sqlite3) {
+    db = new sqlite3.oo1.OpfsDb('/tb_cases.sqlite3');
+  } else {
+    db = new sqlite3.oo1.DB('/tb_cases.sqlite3', 'ct');
+  }
+  try {
+    db.exec(`
+    CREATE TABLE IF NOT EXISTS tb_cases(case_id, date, status)`
+    );
+  } finally {
+    db.close();
+  }
+};
+
+// Get sqlite3 instance
+
+const sqlite3 = await sqlite3InitModule({
+  print: log,
+  printErr: error,
+}).then((sqlite3) => {
+  console.log('Create sqlite db if not already exists');
+  try {
+    return sqlite3
+  } catch (err) {
+    error(err.name, err.message);
+  }
+});
+
+// run the initial starting script
+start(sqlite3);
 
 
 
