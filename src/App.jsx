b6dad24a4d256cc5287613c698f7c6ffa281e1e7
Collin Brown <collin.j.brown95@gmail.com>
2023-09-12 10:13:16 -0400

    refactor(worker.js): create worker event listener for connecting to sqlite database

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,37 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 import MyWorker from './worker?worker&inline'
 
-import sqlite3InitModule from '@sqlite.org/sqlite-wasm';
+const worker = new MyWorker();
 
-const start = function (sqlite3) {
-  console.log('Running SQLite3 version', sqlite3.version.libVersion);
-  const db = new sqlite3.oo1.DB('/mydb.sqlite3', 'ct');
-  try {
-    console.log('Creating a table...');
-    db.exec('CREATE TABLE IF NOT EXISTS t(a,b)');
-    console.log('Insert some data using exec()...');
-    for (let i = 20; i <= 25; ++i) {
-      db.exec({
-        sql: 'INSERT INTO t(a,b) VALUES (?,?)',
-        bind: [i, i * 2],
-      });
-    }
-    console.log('Query data with exec()...');
-    db.exec({
-      sql: 'SELECT a FROM t ORDER BY a LIMIT 3',
-      callback: (row) => {
-        console.log(row);
-      },
-    });
-  } finally {
-    db.close();
+worker.addEventListener("message", async event => {
+  console.log(event.data.type)
+  if (event.data.type === "sqliteworkerResponse") {
+    console.log('event from sqlite worker: ', event);
   }
-};
+})
 
 function App() {
   const [count, setCount] = useState(0)
-  useEffect(() => {
-    console.log('Loading and initializing SQLite3 module...');
-    sqlite3InitModule({
-      print: console.log,
-      printErr: console.error,
-    }).then((sqlite3) => {
-      try {
-        console.log('Done initializing. Running demo...');
-        start(sqlite3);
-      } catch (err) {
-        console.error(err.name, err.message);
-      }
-    });
-    // const worker = new Worker('/worker', { type: 'module' });
-    const worker = new MyWorker();
-    worker.onmessage = (e) => {
-      e.data.type === 'log' ? console.log(e.data.payload) : console.log(e.data.payload);
-    };
-  }, []);  // Ensure useEffect hook runs only once.
+
+
+
+  const handleButtonClick = function (event) {
+    worker.postMessage(event);
+  }
+
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => {
+          setCount((count) => count + 1)
+          handleButtonClick("test")
+        }
+        }
+        >
           count is {count}
         </button>
         <p>
