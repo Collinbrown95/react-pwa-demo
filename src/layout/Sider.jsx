985edc9c30e7ac6d8e42da8298e38fd7d0b460e7
Collin Brown <collin.j.brown95@gmail.com>
2023-09-12 14:37:08 -0400

    tidy(Sider.jsx): remove debug comments

--- src/layout/Sider.jsx
+++ src/layout/Sider.jsx
@@ -11,10 +11,10 @@ const CustomSider = () => {
             breakpoint="lg"
             collapsedWidth="0"
             onBreakpoint={(broken) => {
-                console.log(broken);
+                // console.log(broken);
             }}
             onCollapse={(collapsed, type) => {
-                console.log(collapsed, type);
+                // console.log(collapsed, type);
             }}
         >
             <div className="demo-logo-vertical" />
